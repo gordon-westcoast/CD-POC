b89fc146174b83516731dba40c78f3b2022a0704
gordon-westcoast <47980000+gordon-westcoast@users.noreply.github.com>
2019-04-30 13:41:23 +0100

    Update cucumberJava.java

--- src/test/java/cucumberJava/cucumberJava.java
+++ src/test/java/cucumberJava/cucumberJava.java
@@ -3,10 +3,16 @@ package cucumberJava;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
+import static org.junit.Assert.*;
+import static sun.misc.Version.print;
 
 public class cucumberJava {
     WebDriver driver = null;
@@ -17,21 +23,22 @@ public class cucumberJava {
         driver = new FirefoxDriver();
     }
 
-    @When("^I navigate to the home page$")
-    public void goToHomePage() {
-        driver.navigate().to("http://bnwci01.westcoast.co.uk/");
+    @When("^I navigate to the OPG login page$")
+    public void goToLoginPage() {
+        driver.navigate().to("http://bnwci01.westcoast.co.uk:8080/opg");
     }
 
-    @Then("^The header label should exist$")
-    public void loginButton() {
-        if(driver.findElement(By.cssSelector("html body nav.navbar.navbar-inverse.navbar-fixed-top div.container div.navbar-header a.navbar-brand")).isEnabled()) {
-            System.out.println("Test 1 Pass");
-        } else {
-            System.out.println("Test 1 Fail");
-        }
+    @Then("^The header label should read \"(.*?)\"$")
+    public void loginButton(String headerText) {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".defaultBG2Image > p:nth-child(1) > font:nth-child(1) > label:nth-child(1)")));
+        WebElement header = driver.findElement(By.cssSelector(".defaultBG2Image > p:nth-child(1) > font:nth-child(1) > label:nth-child(1)"));
+        String text1 = header.getText().trim();
+        System.out.print("Comparing expected: "+headerText+" to found "+text1);
+        assertEquals(text1, headerText);
         try {
             driver.quit();
+        } catch (Exception e) {
         }
-        catch(Exception e){}
     }
 }
